229999820a1e4992f5427c951680d8adbae0aafb
shveta05 <91688905+shveta05@users.noreply.github.com>
2023-08-12 23:36:47 +0530

    first commit

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,45 +1,75 @@
 let resumeData = {
-    "name": "Shveta",
-    "role": "Student",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/shveta-sirsat-b2220a214",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"https://github.com/shveta05",
-          "className":"fa fa-github"
-        },
-      ],
-    "aboutme":"I am currently a third year student at SGGSIE&t and pursuing my B.Tech from here.I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"Nanded, Maharashtra, India",
-    "education":[
-      {
-        "UniversityName":"Shri Guru Gobind Singh Ji Institute of Engineering and Technology",
-        "Branch": "Information Tecnology",
-        "Year": "Third Year",
+  "name": "Shveta",
+  
+  "role": "Student",
+    
+  "roleDescription":
+    "I like dabbling in various parts of frontend development and like to learn about new technologies or simply play games in my free time.",
+  
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/shveta-sirsat-b2220a214",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "https://github.com/shveta05",
+      "className": "fa fa-github"
+    },
+  ],
+  
+  "aboutme": "I am currently a third year student at SGGSIE&t and pursuing my B.Tech from here.I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  "address": "Nanded, Maharashtra, India",
+  
+  "education": [
+    {
+      "UniversityName": "Shri Guru Gobind Singh Ji Institute of Engineering and Technology",
+      "Branch": "Information Technology",
+      "Year": "Third Year"
         
-      },
-      {
-        "YearOfPassing":"2024",
+    },{
+      "YearOfPassing": "2024",
         
-      }
+    },
+    {
+      "UniversityName": "Sarosh Junior College",
+      "Branch":"HSC"
+    },{
+      "YearOfPassing": "2019"
+        
+    },
+    {
+      "UniversityName": "Sharda Vidya Mandir",
+      "Branch":"SSC"
+        
+    },{
+      "YearOfPassing": "2017"
+        
+    }
   ],
     
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
+  "skillsDescription": "Your skills here",
+  "skills": [
+    {
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "Reactjs"
+    },
+    {
+      "skillname": "Problem Solving"
+    },
+    {
+      "skillname":"DSA"
+    },
+    {
+      "skillname":"C++"
+    }
+
     ],
     "testimonials":[
       {
